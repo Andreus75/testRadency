2e1a5b16910352d53b4909229dd34b3fbb54380f
Andreus75 <t_andriy@ukr.net>
2022-01-12 16:57:24 +0200

    test task.fix.

--- Main.js
+++ Main.js
@@ -3,24 +3,21 @@ const chooseOptimalDistance = (t, k, ls) => {
     if (ls.length < 1 || k > ls.length) {
         return null;
     }
-    // ls = ls.sort((a,b) => a-b);
 
     for (let i = 0; i < ls.length - k + 1; i++) {
         if (k === 1) {
             if (x <= ls[i] && ls[i] <= t){
                 x = ls[i];
-                break;
             }
         }
-       for (let j = i+1; j < ls.length - k + 2; j++) {
+       for (let j = i + 1; j < ls.length - k + 2; j++) {
            if (k === 2) {
                let sum2 = ls[i] + ls[j];
                if (x <= sum2 && sum2 <= t) {
                    x = sum2;
-                   break;
                }
            }
-           for (let l = j+1; l < ls.length; l++) {
+           for (let l = j + 1; l < ls.length; l++) {
                if (k === 3) {
                    let sum3 =ls[i] + ls[j] + ls[l];
                    if (x <= sum3 && sum3 <= t){
@@ -35,9 +32,7 @@ const chooseOptimalDistance = (t, k, ls) => {
 }
 
 chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61]); //173
-chooseOptimalDistance(163, 3, [50]);
-chooseOptimalDistance(331, 2, [ 91, 74, 73, 85, 73, 81, 87 ]);
+chooseOptimalDistance(163, 3, [50]);//null
+chooseOptimalDistance(331, 2, [ 91, 74, 73, 85, 73, 81, 87 ]);//178
 
 
-// [51,56,58], [51,56,59], [51,56,61], [51,58,59], [51, 58,61], [51,59,61], [56,58,59], [56,58,61], [56,59,61], [58,59,61]
-//     165,         166,       168,       168,          170,         171,       173,         175,       176,        178
